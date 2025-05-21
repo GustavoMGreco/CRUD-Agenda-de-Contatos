89a8a2bd0376e21004c5980e9fa3b6a2f7aade63
Gustavo M Greco <gustavomonteirogreco@gmail.com>
2025-05-21 17:39:19 -0300

    implementar funcionalidades de CRUD para contatos na aplicação flask

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 
 app = Flask(__name__)
+
 
 def conectar():
     return pymysql.connect(
@@ -12,18 +13,84 @@ def conectar():
         cursorclass=pymysql.cursors.DictCursor
     )
 
-@app.route('/')
 def listarContatos():
-    return
+    conexao = conectar()
+    with conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('SELECT * FROM contatos')
+            return cursor.fetchall()
 
-def adicionarContato():
-    return
+def adicionarContato(dados):
+    conexao = conectar()
+    with conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('INSERT INTO contatos(nome, telefone, email) VALUES (%s, %s, %s)', (dados['nome'], dados['telefone'], dados['email']))
+            conexao.commit()
 
-def editarContato():
-    return
+def buscarContato(id):
+    conexao = conectar()
+    with conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('SELECT * FROM contatos WHERE id = %s', (id,))
+            contato = cursor.fetchone()
+            return contato
 
-def buscarContato():
-    return
+def editarContato(id, dados):
+    conexao = conectar()
+    with conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('UPDATE contatos SET nome=%s, telefone=%s, email=%s WHERE id=%s', (dados['nome'], dados['telefone'], dados['email'], id))
+            conexao.commit()
+            return cursor.rowcount
 
-def excluirContato():
-    return
+def excluirContato(id):
+    conexao = conectar()
+    with conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('DELETE FROM contatos WHERE id = %s', (id,))
+            conexao.commit()
+            return cursor.rowcount
+
+
+@app.route('/')
+def listar():
+    contatos = listarContatos()
+    return render_template('lista.html', contatos=contatos)
+
+@app.route('/novo')
+def novoContato():
+    return render_template('formulario.html', contato={})
+
+@app.route('/adicionar', methods=['POST'])
+def adicionar():
+    dados = {
+        'nome': request.form['nome'],
+        'telefone': request.form['telefone'],
+        'email': request.form['email']
+    }
+    adicionarContato(dados)
+    return redirect(url_for('listar'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    contato = buscarContato(id)
+    return render_template('formulario.html', contato=contato)
+
+@app.route('/atualizar/<int:id>', methods=['POST'])
+def atualizar(id):
+    dados = {
+        'nome': request.form['nome'],
+        'telefone': request.form['telefone'],
+        'email': request.form['email']
+    }
+    editarContato(id, dados)
+    return redirect(url_for('listar')) 
+
+@app.route('/excluir/<int:id>')
+def excluir(id):
+    excluirContato(id)
+    return redirect(url_for('listar'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
